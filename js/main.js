43301e8367d6b33faf7e7a31e5e1539322cc976d
Thomas Heaton <thomasaheaton@gmail.com>
2015-11-13 22:01:02 -0500

    setting up files

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
-import $ from 'jquery';
-import _ from 'underscore';
-import moment from 'moment';
+import angular from 'angular';
+import 'angular-ui-router';
+import HomeController from './controllers/home.controller';
+import LogController from './controllers/log.controller';
+import TravellerController from './controllers/traveller.controller';
 
-console.log('Hello, World');
+angular
+  .module('app', ['ui-router']);
